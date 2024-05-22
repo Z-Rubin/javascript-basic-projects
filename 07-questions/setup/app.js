dfe265f43ea48a9ba55411402bcb1fef3657f8ab
Z-Rubin <86024631+Z-Rubin@users.noreply.github.com>
2024-05-22 22:16:30 +0200

    questions done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
 // traversing the dom
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question){
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener("click", function(){
+        questions.forEach(function(item){
+            item.classList.remove("show-text")
+        });
+        question.classList.toggle("show-text");
+    })
+});
+
+
+//const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function(btn){
+//     btn.addEventListener("click", function(e){
+//         const parent = e.currentTarget.parentElement.parentElement;
+//         parent.classList.toggle("show-text");
+//     });
+// });
