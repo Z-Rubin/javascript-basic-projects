6ca104f665f062fb675d5fc674f41a38eb7fd3eb
Z-Rubin <86024631+Z-Rubin@users.noreply.github.com>
2024-05-26 10:38:57 +0200

    countdown timer task done

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,46 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".give-away");
+const deadline = document.querySelector(".deadline");
+
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2025, 5, 18, 12);
+
+giveaway.textContent = `giveaway ends on ${weekdays[futureDate.getDay()-1]}, 
+  ${futureDate.getDate()} 
+  ${months[futureDate.getMonth()-1]} 
+  ${futureDate.getFullYear()}, 
+  ${futureDate.toLocaleString()}`
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const dateDif = futureDate-today;
+  const oneDay = 24*60*60*1000;
+  const oneHour = 60*60*1000;
+  const oneMinute = 60*1000;
+
+  let days = Math.floor(dateDif/oneDay);
+  let hours = Math.floor(dateDif%oneDay/oneHour);
+  let minutes = Math.floor(dateDif%oneHour/oneMinute);
+  let seconds = Math.floor(dateDif%oneMinute/1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  items.forEach(function(item, index){
+    item.innerHTML = format(values[index]);
+  });
+}
+
+function format(item){
+  if (item < 10)
+    return item = `0${item}`;
+  else
+  return item
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
+
